b6f50154cf54481a429f610fa5e2832d8c24ab81
Shoxys <n11961180@qut.edu.au>
2025-02-25 17:48:04 +1000

    Updated Shapes2D class, Circle class and partially Equilateral Triangle class
    Signed-off-by: Shoxys <n11961180@qut.edu>

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -8,9 +8,9 @@ package shapes;
  * contain any vertices
  */
 
-public class Circle  {
+public class Circle extends Shape2D  {
 
-
+    private double radius;
 
     /**
      * Constructor for Circle shape object
@@ -18,10 +18,42 @@ public class Circle  {
      * @param radius The radius of the circle created
      */
     public Circle(Point centre, double radius) {
-
+        super(centre);
+        this.radius = radius;
     }
 
+    public double getRadius() {
+        return radius;
+    }
 
+    public void setRadius (double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public boolean containsPoint(Point point) {
+        double dx = point.getXCord() - centre.getXCord();
+        double dy = point.getYCord() - centre.getYCord();
+
+        double distance  = Math.sqrt(dx * dx * dy * dy);
+
+        return distance <= radius;
+    }
+
+    @Override
+    public Point[] getVertices() {
+        return new Point[0];
+    }
 
 
 }
--- src/shapes/EquilateralTriangle.java
+++ src/shapes/EquilateralTriangle.java
@@ -8,9 +8,9 @@ package shapes;
  * contains 3 vertices
  */
 
-public class EquilateralTriangle  {
+public class EquilateralTriangle extends Shape2D  {
 
-
+    private double sideLength;
 
     /**
     * Constructor for Equilateral Triangle  shape object
@@ -18,8 +18,54 @@ public class EquilateralTriangle  {
     * @param sideLength The length of each side (all same as equilateral)
     */
     public EquilateralTriangle(Point centre, double sideLength) {
-
+        super(centre);
+        this.sideLength = sideLength;
     }
+
+    public double getSideLength() {
+        return sideLength;
+    }
+
+    private void setSideLength(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getArea() {
+        return (Math.sqrt(3) / 4) * sideLength * sideLength;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 3 * sideLength;
+    }
+
+    @Override
+    public boolean containsPoint(Point point) {
+        double dx = point.getXCord() - centre.getXCord();
+        double dy = point.getYCord() - centre.getYCord();
+
+        if (dy <= Math.sqrt(3) * (dx + sideLength / 3) &&
+            dy <= -(Math.sqrt(3) * (dx - sideLength / 3)) &&
+            dy >= - ((Math.sqrt(3) / 6) * sideLength)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public Point[] getVertices() {
+        double eqRXCord = centre.getXCord();
+        double eqTYCord = centre.getYCord();
+
+        Point Vertice1 = new Point(eqRXCord, eqRXCord * ((Math.sqrt(3) / 3)) * sideLength);
+        Point Vertice2 = new Point(eqRXcord - SideLength / 2, eqTYCord - ((Math.sqrt(3) / 6) * sideLength);
+        Point Vertice3 = new Point(eqRXCord + sideLength / 2, eqTYCord - ((Math.sqrt(3) / 6) * sideLength));
+    }
+
+
 
 
 
